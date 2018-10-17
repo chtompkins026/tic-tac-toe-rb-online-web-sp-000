8a18e1d0e6de84b4bf98b00192e489124c9608f8
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-17 14:27:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,13 +69,13 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+    pos1 = win_combo[0]
+    pos2 = win_combo[1]
+    pos3 = win_combo[2]
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
+    position_1 = board[pos1]
+    position_2 = board[pos2]
+    position_3 = board[pos3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
